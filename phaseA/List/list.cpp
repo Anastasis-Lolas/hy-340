b7a52d191f2ae4fa376092f52bedfd8a2b253664
Alex <naxi@Alex-Desktop>
2025-02-24 20:55:11 +0200

    Print Gen

--- phaseA/List/list.cpp
+++ phaseA/List/list.cpp
@@ -1,26 +1,41 @@
 #include "list.h"
 
-list_token_t * insert_token(list_token_t * head ,int nline,int nToken,char * content,char * type){
+list_token_t *insert_token(list_token_t *head, int nline, int nToken, char *content, char *type)
+{
 
-    list_token_t * new_token = new list_token_t;
+    list_token_t *new_token = new list_token_t;
 
     new_token->numline = nline;
-    new_token->numToken   = nToken;
+    new_token->numToken = nToken;
     new_token->content = content;
     new_token->type = type;
     new_token->next = NULL;
-    
-    list_token_t * temp_head = head;
 
-    if(temp_head == NULL){
+    list_token_t *temp_head = head;
+
+    if (temp_head == NULL)
+    {
         head = new_token;
         return head;
-    }else{
-        while(temp_head->next!= NULL){
+    }
+    else
+    {
+        while (temp_head->next != NULL)
+        {
             temp_head = temp_head->next;
         }
 
         temp_head->next = new_token;
         return head;
+    }
+}
+
+void print(list_token_t *head)
+{
+    list_token_t *itr = head;
+    while (itr != nullptr)
+    {
+        /*print edw*/
+        itr = itr->next;
     }
 }

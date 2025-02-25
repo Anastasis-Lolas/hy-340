86e8266bd0ff3f81f3b0ed1f6e88c00e48ea56f5
Anastasis-Lolas <anastasislolas@gmail.com>
2025-02-25 19:39:16 +0200

    List prints

--- phaseA/List/list.cpp
+++ phaseA/List/list.cpp
@@ -1,36 +1,34 @@
 #include "list.h"
 
-list_token_t *insert_token(list_token_t *head, int nline, int nToken,const string content,const string type)
+list_token_t *insert_token(list_token_t *head, int nline, int nToken,const string content,const string type,const string upper_content,const string arrow)
 {
 
     list_token_t *new_token = new list_token_t;
+    list_token_t *temp_head = head;
 
     new_token->numline = nline;
     new_token->numToken = nToken;
 
     // this will change after since int,const,char,float etc are not enumerated 
-    new_token->arrow = "<--enumerated";
+    new_token->arrow = arrow;
 
     
     new_token->content = content;
 
+    new_token->upper_content = upper_content;
 
     new_token->type = type;
 
-
     new_token->next = NULL;
 
-    list_token_t *temp_head = head;
-    
-    
-
+   
     if (temp_head == NULL)
     {
-        head = new_token;
+        temp_head = new_token;
         
-        print_token(head);
+        print_token(temp_head);
         
-        return head;
+        return temp_head;
     }else
     {
         while (temp_head->next != NULL)
@@ -57,5 +55,11 @@ void print(list_token_t *head)
 
 
 void print_token(list_token_t *node){
-    cout << node->numline<< ": #" << node->numToken << "  " << node->content << endl;
+    cout << node->numline<< ": #" << node->numToken << " \"" << node->content<< "\"  ";
+
+    if(node->type != "ID"){
+        cout <<node ->type<< " " << node->upper_content<< " "<<node->arrow<< endl;
+    }else{
+        cout << node ->type<< "  \"" << node->content<< "\"  "<< node->arrow<< endl;
+    }
 }
--- phaseA/List/list.h
+++ phaseA/List/list.h
@@ -10,12 +10,13 @@ typedef struct alpha_token_t{
     unsigned int numToken;
     string content;
     string type;
+    string upper_content;
     string arrow ;
     struct alpha_token_t * next;
 
 }list_token_t;
 
-list_token_t * insert_token(list_token_t * head ,int nline,int nToken,const string content,const string type);
+list_token_t * insert_token(list_token_t * head ,int nline,int nToken,const string content,const string type,const string upper_content,const string arrow);
 
 void print(list_token_t *head) ;
 

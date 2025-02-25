9cfa9e42f794b9bedf9280eae254c8b005ef8768
Alex <naxi@Alex-Desktop>
2025-02-25 18:10:06 +0200

    rearrange

--- phaseA/List/list.h
+++ phaseA/List/list.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 
 using namespace std;
@@ -10,11 +10,13 @@ typedef struct alpha_token_t{
     unsigned int numToken;
     char * content;
     char * type;
+    char * arrow ;
     struct alpha_token_t * next;
 
 }list_token_t;
 
-list_token_t * insert_token(list_token_t * head ,int nline,int nToken,char * content,char * type);
+list_token_t * insert_token(list_token_t * head ,int nline,int nToken,const char * content,const char * type);
 
+void print(list_token_t *head) ;
 
-
+void print_token(list_token_t * node);

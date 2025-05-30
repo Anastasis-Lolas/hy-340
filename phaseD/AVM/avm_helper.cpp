bb1ee7e0480bfe02e6e0cb7784ae796ab1cb438d
Paraskevi Mourelatou <paraskevimour@gmail.com>
2025-05-30 14:22:55 +0300

    helper

---
+++ phaseD/AVM/avm_helper.cpp
@@ -0,0 +1,36 @@
+#include "avm_helper.h"
+
+
+bool number_tobool (avm_memcell* m) { return m->data.numVal != 0; }
+
+bool string_tobool (avm_memcell* m) { return !(m->data.strVal->empty()); }
+
+bool bool_tobool (avm_memcell* m) { return m->data.boolVal; }
+
+bool table_tobool (avm_memcell* m) { return true; }
+
+bool userfunc_tobool (avm_memcell* m) { return true; }
+
+bool libfunc_tobool (avm_memcell* m) { return true; }
+
+bool nil_tobool (avm_memcell* m) { return false; }
+
+bool undef_tobool (avm_memcell* m) { assert(0); return false; }
+
+bool avm_tobool (avm_memcell* m){
+   
+    assert(m->type >= 0 && m->type < undef_m);
+    return (*toboolFuncs[m->type]) (m);
+}
+
+tobool_func_t toboolFuncs[] = {
+    number_tobool,
+    string_tobool,
+    bool_tobool,
+    table_tobool,
+    userfunc_tobool,
+    libfunc_tobool, 
+    nil_tobool,
+    undef_tobool
+};
+
---
+++ phaseD/AVM/avm_helper.h
@@ -0,0 +1,25 @@
+
+#ifndef AVM_HELPER_H
+#define AVM_HELPER_H
+#include <iostream>
+#include <string>
+#include "memcell_struct.h"
+#include "t-codeLib/t-code.h"
+#include "avm_table.h"
+
+
+typedef bool (*tobool_func_t) (avm_memcell*);
+
+bool number_tobool (avm_memcell* m);
+bool string_tobool (avm_memcell* m);
+bool bool_tobool (avm_memcell* m);
+bool table_tobool (avm_memcell* m);
+bool userfunc_tobool (avm_memcell* m);
+bool libfunc_tobool (avm_memcell* m);
+bool nil_tobool (avm_memcell* m);
+bool undef_tobool (avm_memcell* m);
+
+
+
+
+#endif // AVM_HELPER_H
